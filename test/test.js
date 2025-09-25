7537dd7b3e00c27710b9a1fa5dbcbe023f4fbd39
ritesh <r41723554@gmail.com>
2025-09-25 18:01:11 +0530

    this is what this is

---
+++ test/test.js
@@ -0,0 +1,17 @@
+const app = require("express")();
+const supertest = require("supertest");
+const request = supertest(app);
+
+app.get("/", function(req,res){
+    res.status(200).send("practice with kubernetes");
+})
+
+describe("GET /", function(){
+    it("should return 200 OK",function(done){
+        request.get("/").expect(200).end(function(err,res){
+            if(err) return done(err);
+            done();
+        })
+
+    })
+})
